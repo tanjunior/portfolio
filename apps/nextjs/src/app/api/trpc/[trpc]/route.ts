049cbd1666d164a8384c8eaa85928864af9e2ea6
Spastic <junior.tan@live.com>
2023-10-13 16:50:45 +0800

    trpc
    nextauth to clerk
    next adapter to fetch adapter

--- apps/nextjs/src/app/api/trpc/[trpc]/route.ts
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -1,42 +1,71 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-import { appRouter, createTRPCContext } from "@acme/api";
-import { auth } from "@acme/auth";
+// import { appRouter, createContext } from "@acme/api";
+// // import { auth } from "@acme/auth";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
-/**
- * Configure basic CORS headers
- * You should extend this to match your needs
- */
-function setCorsHeaders(res: Response) {
-  res.headers.set("Access-Control-Allow-Origin", "*");
-  res.headers.set("Access-Control-Request-Method", "*");
-  res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-  res.headers.set("Access-Control-Allow-Headers", "*");
-}
+// /**
+//  * Configure basic CORS headers
+//  * You should extend this to match your needs
+//  */
+// function setCorsHeaders(res: Response) {
+//   res.headers.set("Access-Control-Allow-Origin", "*");
+//   res.headers.set("Access-Control-Request-Method", "*");
+//   res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+//   res.headers.set("Access-Control-Allow-Headers", "*");
+// }
 
-export function OPTIONS() {
-  const response = new Response(null, {
-    status: 204,
+// export function OPTIONS() {
+//   const response = new Response(null, {
+//     status: 204,
+//   });
+//   setCorsHeaders(response);
+//   return response;
+// }
+
+// const handler = (req: Request) => {
+//   const response = fetchRequestHandler({
+//     endpoint: "/api/trpc",
+//     router: appRouter,
+//     req,
+//     createContext,
+//     onError({ error, path }) {
+//       console.error(`>>> tRPC Error on '${path}'`, error);
+//     },
+//   });
+
+//   setCorsHeaders(response);
+//   return response;
+// };
+
+import { appRouter, createContext } from "@acme/api";
+// import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+// export API handler
+const handler = (req: Request) => fetchRequestHandler({
+      endpoint: "/api/trpc",
+      router: appRouter,
+      req,
+      createContext,
+      onError({ error, path }) {
+        console.error(`>>> tRPC Error on '${path}'`, error);
+      },
   });
-  setCorsHeaders(response);
-  return response;
-}
 
-const handler = auth(async (req) => {
-  const response = await fetchRequestHandler({
-    endpoint: "/api/trpc",
-    router: appRouter,
-    req,
-    createContext: () => createTRPCContext({ auth: req.auth, req }),
-    onError({ error, path }) {
-      console.error(`>>> tRPC Error on '${path}'`, error);
-    },
-  });
+  export { handler as GET, handler as POST };
 
-  setCorsHeaders(response);
-  return response;
-});
 
-export { handler as GET, handler as POST };
+// If you need to enable cors, you can do so like this:
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   // Enable cors
+//   await cors(req, res);
+
+//   // Let the tRPC handler do its magic
+//   return createNextApiHandler({
+//     router: appRouter,
+//     createContext,
+//   })(req, res);
+// };
+
+// export default handler;
---
+++ packages/api/src/context.ts
@@ -0,0 +1,47 @@
+import { auth } from "@clerk/nextjs/server";
+import type { inferAsyncReturnType } from "@trpc/server";
+import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+
+import { db } from "@acme/db";
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type AuthSession = {
+  session: {
+    user: {
+      id: string;
+      name?: string;
+      email?: string;
+    };
+  } | null;
+};
+
+function getUserAuth(): AuthSession {
+  const { userId } = auth();
+  if (userId) {
+    return {
+      session: {
+        user: {
+          id: userId,
+        },
+      },
+    } as AuthSession;
+  } else {
+    return { session: null };
+  }
+};
+
+export const createContext = (opts: FetchCreateContextFnOptions) => {
+  const {session} = getUserAuth();
+
+
+  const source = opts.req?.headers.get("x-trpc-source") ?? "unknown";
+  console.log(">>> tRPC Request from", source, "by", session ? session.user.id : "public");
+
+  return {
+    session: session,
+    headers: opts && Object.fromEntries(opts.req.headers),
+    db,
+  };
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
