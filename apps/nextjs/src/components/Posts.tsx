64022a163e038a1b75023c1265f6ccb442e1a4da
Spastic <junior.tan@live.com>
2023-10-14 23:56:24 +0800

    Update Posts.tsx

--- apps/nextjs/src/components/Posts.tsx
+++ apps/nextjs/src/components/Posts.tsx
@@ -1,16 +1,15 @@
-import { api } from "~/utils/server";
+// import { api } from "~/utils/server";
 import CreatePostForm from "./CreatePostForm";
-
-// import PostList from "./PostList";
+import PostList from "./PostList";
 
 export default async function Posts() {
-  const posts = await api.post.all.query();
-  console.log(posts);
+  // const posts = await api.post.all.query();
+  // console.log(posts);
   return (
     <div className="h-[50vh] w-full max-w-2xl overflow-y-scroll">
       <CreatePostForm />
       {/* <PostList posts={posts} /> */}
-      {/* <PostList /> */}
+      <PostList />
     </div>
   );
 }
