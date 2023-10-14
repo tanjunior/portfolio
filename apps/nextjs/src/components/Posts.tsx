64a1115b80b6e26d71cc554b463ad59fdfdddb09
Spastic <junior.tan@live.com>
2023-10-14 23:51:31 +0800

    test2

--- apps/nextjs/src/components/Posts.tsx
+++ apps/nextjs/src/components/Posts.tsx
@@ -1,6 +1,7 @@
 import { api } from "~/utils/server";
 import CreatePostForm from "./CreatePostForm";
-import PostList from "./PostList";
+
+// import PostList from "./PostList";
 
 export default async function Posts() {
   const posts = await api.post.all.query();
@@ -9,7 +10,7 @@ export default async function Posts() {
     <div className="h-[50vh] w-full max-w-2xl overflow-y-scroll">
       <CreatePostForm />
       {/* <PostList posts={posts} /> */}
-      <PostList />
+      {/* <PostList /> */}
     </div>
   );
 }
