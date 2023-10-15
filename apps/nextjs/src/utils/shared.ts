46deaed8fb016cfc0040a73fed661c72e36868cc
Spastic <junior.tan@live.com>
2023-10-15 23:54:11 +0800

    Update shared.ts

--- apps/nextjs/src/utils/shared.ts
+++ apps/nextjs/src/utils/shared.ts
@@ -7,7 +7,7 @@ import { env } from "~/env.mjs";
 
 export const transformer = superjson;
 
-function getBaseUrl() {
+export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
   if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
   return `http://localhost:${env.PORT}`;
