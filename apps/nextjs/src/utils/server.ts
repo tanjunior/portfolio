8094682a7067741eea34681499b1a7e315c39bd1
Spastic <junior.tan@live.com>
2023-10-15 21:33:58 +0800

    change to nexthttplink

--- apps/nextjs/src/utils/server.ts
+++ apps/nextjs/src/utils/server.ts
@@ -2,6 +2,7 @@ import { cookies, headers } from "next/headers";
 // import { currentUser } from "@clerk/nextjs";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 // import { experimental_nextCacheLink as nextCacheLink } from "@trpc/next/app-dir/links/nextCache";
+import { experimental_nextHttpLink as nextHttpLink } from "@trpc/next/app-dir/links/nextHttp";
 import { experimental_createTRPCNextAppDirServer as createTRPCNextAppDirServer } from "@trpc/next/app-dir/server";
 
 // import { appRouter } from "@acme/api";
@@ -46,6 +47,17 @@ export const api = createTRPCNextAppDirServer<AppRouter>({
             process.env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
+        nextHttpLink({
+          batch: true,
+          url: getUrl(),
+          headers() {
+            // Forward headers from the browser to the API
+            return {
+              ...Object.fromEntries(headers()),
+              "x-trpc-source": "nextjs-rsc",
+            };
+          },
+        }),
         // nextCacheLink({
         //   revalidate: 1,
         //   router: appRouter,
@@ -58,19 +70,26 @@ export const api = createTRPCNextAppDirServer<AppRouter>({
         //         cookie: cookies().toString(),
         //         "x-trpc-source": "rsc-invoke",
         //       },
+        //       // headers() {
+        //       //   // Forward headers from the browser to the API
+        //       //   return {
+        //       //     ...Object.fromEntries(headers()),
+        //       //     "x-trpc-source": "nextjs-rsc",
+        //       //   };
+        //       // },
         //     };
         //   },
         // }),
-        httpBatchLink({
-          url: getUrl(),
-          headers() {
-            // Forward headers from the browser to the API
-            return {
-              ...Object.fromEntries(headers()),
-              "x-trpc-source": "nextjs-rsc",
-            };
-          },
-        }),
+        // httpBatchLink({
+        //   url: getUrl(),
+        //   headers() {
+        //     // Forward headers from the browser to the API
+        //     return {
+        //       ...Object.fromEntries(headers()),
+        //       "x-trpc-source": "nextjs-rsc",
+        //     };
+        //   },
+        // }),
       ],
     };
   },
