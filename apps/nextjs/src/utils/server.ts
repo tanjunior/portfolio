4c6f23a9f84d3a5a8d01cf272371b81960c3c82e
Spastic <junior.tan@live.com>
2023-10-15 23:54:30 +0800

    Update server.ts

--- apps/nextjs/src/utils/server.ts
+++ apps/nextjs/src/utils/server.ts
@@ -51,10 +51,6 @@ export const api = createTRPCNextAppDirServer<AppRouter>({
           batch: true,
           url: getUrl(),
           headers() {
-            console.log(
-              "=================================== url ===============================",
-              getUrl(),
-            );
             // Forward headers from the browser to the API
             return {
               ...Object.fromEntries(headers()),
