33ddd65ab25c323b4bb32b94d9c3fc4513ccb0a3
Spastic <junior.tan@live.com>
2023-10-15 19:48:58 +0800

    Update context.ts

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -1,8 +1,12 @@
-import { auth } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
 import type { inferAsyncReturnType } from "@trpc/server";
 import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 
 import { db } from "@acme/db";
+
+interface CreateContextOptions {
+  headers: Headers;
+}
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 type AuthSession = {
@@ -15,13 +19,13 @@ type AuthSession = {
   } | null;
 };
 
-function getUserAuth(): AuthSession {
-  const { userId } = auth();
-  if (userId) {
+async function getUserAuth(): Promise<AuthSession> {
+  const user = await currentUser();
+  if (user) {
     return {
       session: {
         user: {
-          id: userId,
+          id: user.id,
         },
       },
     } as AuthSession;
@@ -30,22 +34,33 @@ function getUserAuth(): AuthSession {
   }
 }
 
-export const createContext = (opts?: FetchCreateContextFnOptions) => {
-  const { session } = getUserAuth();
-
-  const source = opts?.req?.headers.get("x-trpc-source") ?? "unknown";
-  console.log(
-    ">>> tRPC Request from",
-    source,
-    "by",
-    session ? session.user.id : "public",
-  );
+/**
+ * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
+ * it from here.
+ *
+ * Examples of things you may need it for:
+ * - testing, so we don't have to mock Next.js' req/res
+ * - tRPC's `createSSGHelpers`, where we don't have req/res
+ *
+ * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
+ */
+async function createInnerContext({ headers }: CreateContextOptions) {
+  const { session } = await getUserAuth();
+  const source = headers.get("x-trpc-source") ?? "unknown";
+  console.log(">>> tRPC Request from", source, "by", session?.user.id);
 
   return {
-    session: session,
-    headers: opts && Object.fromEntries(opts.req.headers),
+    session,
+    headers,
     db,
   };
-};
+}
+
+export async function createContext(opts: FetchCreateContextFnOptions) {
+  return await createInnerContext({
+    headers: opts.req.headers,
+  });
+}
 
 export type Context = inferAsyncReturnType<typeof createContext>;
+// export type Context = Awaited<ReturnType<typeof createContext>>;
