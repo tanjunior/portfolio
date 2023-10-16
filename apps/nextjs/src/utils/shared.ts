eda5885cc4bb7105ff5d0f1d7c3b94db98ae1058
Spastic <junior.tan@live.com>
2023-10-16 17:10:57 +0800

    Update shared.ts

--- apps/nextjs/src/utils/shared.ts
+++ apps/nextjs/src/utils/shared.ts
@@ -1,32 +1,12 @@
-// import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import superjson from "superjson";
-
-// import type { AppRouter } from "@acme/api";
-
 import { env } from "~/env.mjs";
 
-export const transformer = superjson;
-
 export function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
-  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
-  return `http://localhost:${env.PORT}`;
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (env.VERCEL_URL) return env.VERCEL_URL; // SSR should use vercel url
+
+  return `http://localhost:${env.PORT}`; // dev SSR should use localhost
 }
 
 export function getUrl() {
   return getBaseUrl() + "/api/trpc";
 }
-
-// /**
-//  * Inference helper for inputs.
-//  *
-//  * @example type HelloInput = RouterInputs['example']['hello']
-//  */
-// export type RouterInputs = inferRouterInputs<AppRouter>;
-
-// /**
-//  * Inference helper for outputs.
-//  *
-//  * @example type HelloOutput = RouterOutputs['example']['hello']
-//  */
-// export type RouterOutputs = inferRouterOutputs<AppRouter>;
