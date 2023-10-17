49989b28e5626d07bd006190b53a67f99fa8532d
Spastic <junior.tan@live.com>
2023-10-18 05:13:23 +0800

    Update _layout.tsx

--- apps/expo/src/app/(auth)/_layout.tsx
+++ apps/expo/src/app/(auth)/_layout.tsx
@@ -1,12 +1,82 @@
-import { Redirect, Stack } from "expo-router";
+import { Pressable, Text } from "react-native";
+import { Tabs, useRouter } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function AuthenticatedLayout() {
+import { chatClient, useChatClient } from "~/hooks/useChatClient";
+
+export const LogoutButton = () => {
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const doLogout = async () => {
+    await chatClient.disconnectUser();
+    await signOut();
+    router.replace("/login");
+  };
+
+  return (
+    <Pressable onPress={doLogout} style={{ marginRight: 10 }}>
+      <Ionicons name="log-out-outline" size={24} color={"#fff"} />
+    </Pressable>
+  );
+};
+
+const TabsPage = () => {
   const { isSignedIn } = useAuth();
+  const { clientIsReady } = useChatClient();
 
-  if (!isSignedIn) {
-    return <Redirect href={"/sign-in"} />;
+  if (!clientIsReady) {
+    return <Text>Loading chat ...</Text>;
   }
 
-  return <Stack />;
-}
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#6c47ff",
+        },
+        headerTintColor: "#fff",
+      }}
+    >
+      <Tabs.Screen
+        name="(home)"
+        options={{
+          headerShown: false,
+          // headerTitle: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+          tabBarLabel: "Home",
+        }}
+        redirect={!isSignedIn}
+      />
+      <Tabs.Screen
+        name="(app)"
+        options={{
+          headerShown: false,
+          // headerTitle: "Chat",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubble-outline" size={size} color={color} />
+          ),
+          tabBarLabel: "Chat",
+        }}
+        redirect={!isSignedIn}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "My Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+          tabBarLabel: "My Profile",
+          headerRight: () => <LogoutButton />,
+        }}
+        redirect={!isSignedIn}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsPage;
