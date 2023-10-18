2814a9ace286cdba368e62409470300550afb93e
Spastic <junior.tan@live.com>
2023-10-18 23:21:17 +0800

    Create index.ts

---
+++ apps/nextjs/src/utils/index.ts
@@ -0,0 +1,38 @@
+import { clsx } from "clsx";
+import type { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import type { links } from "./data";
+
+export type SectionName = (typeof links)[number]["name"];
+
+export const validateString = (
+  value: unknown,
+  maxLength: number,
+): value is string => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
+
+  return true;
+};
+
+export const getErrorMessage = (error: unknown): string => {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
+
+  return message;
+};
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
