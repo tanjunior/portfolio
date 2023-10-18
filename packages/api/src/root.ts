b33978d1c2c37f3514b8491ff0b472625fdee2f8
Spastic <junior.tan@live.com>
2023-10-18 23:18:15 +0800

    Update root.ts

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
+import { emailRouter } from "./router/email";
 import { postRouter } from "./router/post";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  email: emailRouter,
 });
 
 // export type definition of API
