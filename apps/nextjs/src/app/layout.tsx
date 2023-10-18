1e67098c2c8ec5a14070bbedf5cf515743739b2d
Spastic <junior.tan@live.com>
2023-10-18 23:22:50 +0800

    Update layout.tsx

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -4,9 +4,16 @@ import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 
 import { headers } from "next/headers";
-import { ClerkProvider } from "@clerk/nextjs";
+import Footer from "@/portfolio/footer";
+import Header from "@/portfolio/header";
+import ThemeSwitch from "@/portfolio/theme-switch";
+import { Toaster } from "@/ui/toaster";
 
-import { env } from "~/env.mjs";
+// import { ClerkProvider } from "@clerk/nextjs";
+
+import ActiveSectionContextProvider from "~/contexts/active-section-context";
+import ThemeContextProvider from "~/contexts/theme-context";
+// import { env } from "~/env.mjs";
 import { TRPCReactProvider } from "./providers";
 
 const fontSans = Inter({
@@ -37,15 +44,45 @@ export const metadata: Metadata = {
   // },
 };
 
-export default function Layout(props: { children: React.ReactNode }) {
+// export default function Layout(props: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <body className={["font-sans", fontSans.variable].join(" ")}>
+//         <ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+//           <TRPCReactProvider headers={headers()}>
+//             {props.children}
+//           </TRPCReactProvider>
+//         </ClerkProvider>
+//         <Toaster />
+//       </body>
+//     </html>
+//   );
+// }
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body className={["font-sans", fontSans.variable].join(" ")}>
-        <ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${fontSans.className} relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}
+      >
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
+        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <ThemeContextProvider>
           <TRPCReactProvider headers={headers()}>
-            {props.children}
+            <ActiveSectionContextProvider>
+              <Header />
+              {children}
+              <Footer />
+
+              <Toaster />
+              <ThemeSwitch />
+            </ActiveSectionContextProvider>
           </TRPCReactProvider>
-        </ClerkProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
