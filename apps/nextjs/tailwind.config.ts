08f2a6bb537a7a9a4ee960725c7a5abcc0b0ab41
Spastic <junior.tan@live.com>
2023-10-18 23:20:04 +0800

    Update tailwind.config.ts

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -3,6 +3,11 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@acme/tailwind-config";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   presets: [baseConfig],
 } satisfies Config;
