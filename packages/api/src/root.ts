c6fa68b3db29e49400d41771abe38b3d5792da1a
Spastic <junior.tan@live.com>
2023-10-22 14:44:32 +0800

    remove uploadthing router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,14 +1,12 @@
 import { emailRouter } from "./router/email";
 import { postRouter } from "./router/post";
 import { todoRouter } from "./router/todo";
-import { uploadthingRouter } from "./router/uploadthing";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
   email: emailRouter,
   todo: todoRouter,
-  uploadthing: uploadthingRouter,
 });
 
 // export type definition of API
