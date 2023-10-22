c1b1eeff54d0860e79adf1d01436d1567312e3fb
Spastic <junior.tan@live.com>
2023-10-23 04:03:01 +0800

    Update root.ts

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,9 +1,11 @@
+import { authRouter } from "./router/auth";
 import { emailRouter } from "./router/email";
 import { postRouter } from "./router/post";
 import { todoRouter } from "./router/todo";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
+  auth: authRouter,
   post: postRouter,
   email: emailRouter,
   todo: todoRouter,
