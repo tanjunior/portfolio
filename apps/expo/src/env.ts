62312785c2d739565675f1e030f9874b6d62c5dd
Spastic <junior.tan@live.com>
2023-10-22 23:16:59 +0800

    Update env.ts

--- apps/expo/src/env.ts
+++ apps/expo/src/env.ts
@@ -1,5 +1,10 @@
 import { createEnv } from "@t3-oss/env-core";
+import Constants from "expo-constants";
 import { z } from "zod";
+
+const extra = Constants.expoConfig?.extra!
+
+console.log("extra", extra);
 
 export const env = createEnv({
   clientPrefix: "EXPO_PUBLIC_",
@@ -9,9 +14,11 @@ export const env = createEnv({
     EXPO_PUBLIC_API_URL: z.string().url(),
   },
   runtimeEnv: {
-    EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    EXPO_PUBLIC_STREAM_API_KEY: process.env.EXPO_PUBLIC_STREAM_API_KEY,
-    EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+    // EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    // EXPO_PUBLIC_STREAM_API_KEY: process.env.EXPO_PUBLIC_STREAM_API_KEY,
+    // EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+    EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: extra.CLERK_PUBLISHABLE_KEY,
+    EXPO_PUBLIC_STREAM_API_KEY: extra.STREAM_API_KEY,
+    EXPO_PUBLIC_API_URL: extra.API_URL,
   },
 });
