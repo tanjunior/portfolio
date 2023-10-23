14b24f0198ef3c1eacebfd1dbfd46886bbc0f353
Spastic <junior.tan@live.com>
2023-10-24 00:20:44 +0800

    Update post.ts

--- packages/db/schema/post.ts
+++ packages/db/schema/post.ts
@@ -1,11 +1,13 @@
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { mysqlTable, serial, timestamp, varchar } from "drizzle-orm/mysql-core";
 import { createInsertSchema } from "drizzle-zod";
 
+import { user } from "./user";
+
 export const post = mysqlTable("posts", {
   id: serial("id").primaryKey(),
-  userId: varchar("user_id", { length: 64 }).notNull(),
+  authorId: varchar("author_id", { length: 64 }).notNull(),
   content: varchar("content", { length: 256 }).notNull(),
   imageUrl: varchar("image_url", { length: 256 }),
   createdAt: timestamp("created_at")
@@ -13,6 +15,13 @@ export const post = mysqlTable("posts", {
     .notNull(),
   updatedAt: timestamp("updatedAt").onUpdateNow(),
 });
+
+export const postRelations = relations(post, ({ one }) => ({
+  author: one(user, {
+    fields: [post.authorId],
+    references: [user.id],
+  }),
+}));
 
 export type SelectPost = InferSelectModel<typeof post>;
 export type InsertPost = InferInsertModel<typeof post>;
