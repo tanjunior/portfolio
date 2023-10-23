b1c258bfb5da1fe8c439e886a62755e5d91caf45
Spastic <junior.tan@live.com>
2023-10-24 00:48:38 +0800

    Update user.ts

--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -1,10 +1,21 @@
 import { z } from "zod";
 
+import { and, desc, eq, schema } from "@acme/db";
 import { insertUserSchema, user } from "@acme/db/schema/user";
 
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
+  get: protectedProcedure
+    .input(z.string().min(1))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.db.query.user.findFirst({
+        where: eq(schema.user.id, input),
+      });
+
+      return user;
+    }),
+
   create: publicProcedure
     .input(insertUserSchema)
     .mutation(async ({ ctx, input }) => {
