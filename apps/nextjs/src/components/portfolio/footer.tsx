3e9ac0e3e6527f8bc2a1f5948aaa8fd816621688
Spastic <junior.tan@live.com>
2023-10-30 12:49:38 +0800

    Update footer.tsx

--- apps/nextjs/src/components/portfolio/footer.tsx
+++ apps/nextjs/src/components/portfolio/footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; 2023 Tan Jing Ren. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        React & Next.js 13(App Router & React Server Components), TypeScript,
+        Tailwind CSS, Framer Motion, Resend, Vercel hosting.
       </p>
     </footer>
   );
