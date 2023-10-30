bc5af076a35f01fbf3e548875c41af1624f8bdf9
Spastic <junior.tan@live.com>
2023-10-30 12:45:54 +0800

    Update about.tsx

--- apps/nextjs/src/components/portfolio/about.tsx
+++ apps/nextjs/src/components/portfolio/about.tsx
@@ -20,30 +20,31 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        I am always fueled by my curiosity of how things work, even when I was a
+        just kid. I can still remember disassembling a brand new toy to find out
+        how each moving part works and then putting it back together.
+      </p>
+
+      <p className="mb-3">
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
+        feeling of finally figuring out a solution to a problem. And turning
+        someone else wireframe design into all full functioning and intereactive
+        UI. My core stack is{" "}
+        <span className="font-medium">React, Next.js, tRPC, and Drizzle</span>.
+        I am also familiar with Flutter and C#. I am always looking to learn new
+        technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a web
         developer.
       </p>
 
       <p>
         <span className="italic">When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and playing with my dog. I also
-        enjoy <span className="font-medium">learning new things</span>. I am
-        currently learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I&apos;m
-        also learning how to play the guitar.
+        watching movies, and playing games. I also enjoy{" "}
+        <span className="font-medium">making things myself</span>. I own a 3D
+        printer where I print out my own designs that I have created in{" "}
+        <span className="font-medium">CAD software</span>. I&apos;m also
+        learning how to make games.
       </p>
     </motion.section>
   );
