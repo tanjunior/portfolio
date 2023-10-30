0458434111b3c976ee1c7c1c2306a4844c5b584d
Spastic <junior.tan@live.com>
2023-10-30 12:45:28 +0800

    Update data.ts

--- apps/nextjs/src/utils/data.ts
+++ apps/nextjs/src/utils/data.ts
@@ -1,7 +1,7 @@
 import React from "react";
-import corpcommentImg from "$/corpcomment.png";
-import rmtdevImg from "$/rmtdev.png";
-import wordanalyticsImg from "$/wordanalytics.png";
+import appImg from "$/app.png";
+import chatImg from "$/chat.png";
+import todoImg from "$/todo.png";
 
 import { Icons } from "~/components/icons";
 
@@ -34,52 +34,68 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    title: "Diploma",
+    location: "Singapore",
+    description: " Diploma in Information Technology",
     icon: React.createElement(Icons.graduationCap),
-    date: "2019",
+    date: "2014",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Operations Specialist",
+    location: "Singapore",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked as an Operations Specialist at NCS Pte Ltd for 7 years to save up for my degree.",
     icon: React.createElement(Icons.briefcase),
-    date: "2019 - 2021",
+    date: "2015",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Bachelor's Degree",
+    location: "Singapore",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(Icons.react),
-    date: "2021 - present",
+      "Bachelor of Information Technology in Games Software Design and Production and Mobile and Web Application Development",
+    icon: React.createElement(Icons.graduationCap),
+    date: "2023",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    title: "Todo",
+    description: "Todo App with type-safe API.",
+    tags: [
+      "TypeScript",
+      "React",
+      "Next.js",
+      "MySQL",
+      "Tailwind",
+      "Dizzle",
+      "TRPC",
+    ],
+    imageUrl: todoImg,
+    path: "/todo",
   },
   {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    title: "Chat",
+    description: "Real-time chat app using golang websocket server.",
+    tags: ["TypeScript", "React", "Tailwind", "WebSocket", "Golang"],
+    imageUrl: chatImg,
+    path: "/chat",
   },
   {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    title: "Social Media App",
+    description: "A social media app with feeds and chat.",
+    tags: [
+      "TypeScript",
+      "React Native",
+      "expo",
+      "MySQL",
+      "Tailwind",
+      "Clerk",
+      "getstream",
+      "uploadthing",
+    ],
+    imageUrl: appImg,
+    path: "https://expo.dev/%40juniortan/expo?serviceType=eas&distribution=expo-go&scheme=&channel=main&sdkVersion=49.0.0",
   },
 ] as const;
 
@@ -88,19 +104,15 @@ export const skillsData = [
   "CSS",
   "JavaScript",
   "TypeScript",
+  "Golang",
   "React",
   "Next.js",
   "Node.js",
   "Git",
   "Tailwind",
   "Prisma",
+  "Dizzle",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
   "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
 ] as const;
