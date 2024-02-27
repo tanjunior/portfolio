074d5eb0893f62e8f89c58337a3ca614a1fc86bf
Spastic <junior.tan@live.com>
2024-02-27 15:58:21 +0800

    Update route.ts

--- apps/nextjs/src/app/api/keep-alive/route.ts
+++ apps/nextjs/src/app/api/keep-alive/route.ts
@@ -4,13 +4,18 @@ import { db, eq, schema, sql } from "@acme/db";
 
 export async function handler() {
   try {
-    const update = await db
+    await db
       .update(schema.keepAlive)
       .set({
         count: sql`${schema.keepAlive.count} + 1`,
       })
       .where(eq(schema.keepAlive.id, "1"));
-    return NextResponse.json(update.time);
+
+    const data = await db.query.keepAlive.findFirst({
+      where: eq(schema.keepAlive.id, "1"),
+    });
+
+    return NextResponse.json(data?.count);
   } catch (error) {
     return NextResponse.json({ error });
   }
