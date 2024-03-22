256e06860c87b8ab130876690c318843860cc102
Spastic <junior.tan@live.com>
2024-03-22 12:03:25 +0800

    change to neondnb

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,20 +1,20 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as keepAlive from "./schema/keep_alive";
 import * as post from "./schema/post";
 import * as todo from "./schema/todo";
 import * as user from "./schema/user";
 
-export const schema = { ...post, ...todo, ...user, ...keepAlive };
+export const schema = {
+  ...post,
+  ...todo,
+  ...user,
+  ...keepAlive,
+};
 
 export * from "drizzle-orm";
 
-
 const sql = neon(process.env.DATABASE_URL!);
 
-
-export const db = drizzle(
-  sql,
-  { schema },
-);
+export const db = drizzle(sql, { schema });
